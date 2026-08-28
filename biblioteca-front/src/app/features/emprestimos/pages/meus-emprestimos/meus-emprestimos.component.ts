5214c0b8d1fdcec13c130edccbbe97ddeffb1cb6
julio <juliocesar.gm6000@gmail.com>
2026-08-28 09:09:30 -0300

    implementação meusEmprestimos com paginação

--- biblioteca-front/src/app/features/emprestimos/pages/meus-emprestimos/meus-emprestimos.component.ts
+++ biblioteca-front/src/app/features/emprestimos/pages/meus-emprestimos/meus-emprestimos.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { EmprestimoService } from '../../services/emprestimo.service';
 import { MeuEmprestimo } from '../../models/meu-emprestimo.model';
-import { Pagina } from '../../../../models/pagina.model';
-
+import { Pagina } from 'src/app/models/pagina.model';
 
 @Component({
   selector: 'app-meus-emprestimos',
@@ -11,12 +11,56 @@ import { Pagina } from '../../../../models/pagina.model';
 })
 export class MeusEmprestimosComponent implements OnInit {
 
+  emprestimos: MeuEmprestimo[] = [];
+
+  colunas: string[] = ['livro','dataEmprestimo','vencimento','retorno','dias','status'];
+
+  paginaAtual = 0;
+  itensPorPagina = 10;
+  totalPaginas = 0;
+
+  carregando = false;
 
   constructor(
     private emprestimoService: EmprestimoService
   ) {}
 
   ngOnInit(): void {
+    this.buscarEmprestimos();
   }
 
+  buscarEmprestimos(): void {
+    this.carregando = true;
+
+    this.emprestimoService.meusEmprestimos(this.paginaAtual,this.itensPorPagina)
+      .subscribe({
+        next: (resposta: Pagina<MeuEmprestimo>) => {
+
+          this.emprestimos = resposta.content;
+          this.paginaAtual = resposta.paginaAtual;
+          this.totalPaginas = resposta.totalPaginas;
+
+          this.carregando = false;
+        },
+
+        error: (erro) => {console.error('Erro ao buscar meus empréstimos:',erro);
+
+          this.carregando = false;
+        }
+      });
+  }
+
+  paginaAnterior(): void {
+    if (this.paginaAtual > 0) {
+      this.paginaAtual--;
+      this.buscarEmprestimos();
+    }
+  }
+
+  proximaPagina(): void {
+    if (this.paginaAtual < this.totalPaginas - 1) {
+      this.paginaAtual++;
+      this.buscarEmprestimos();
+    }
+  }
 }
